9f88a52d89c06085dc16b49c27b212211642a67a
eejai42 <ej@ssot.me>
2021-04-17 19:02:09 -0500

    Add project files.

---
+++ Never-Not-Numlock/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this._closeButton = new System.Windows.Forms.Button();
+            this._numlockStatusLabel = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this._runAtStartup = new System.Windows.Forms.CheckBox();
+            this._hideButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _closeButton
+            // 
+            this._closeButton.Location = new System.Drawing.Point(832, 33);
+            this._closeButton.Name = "_closeButton";
+            this._closeButton.Size = new System.Drawing.Size(181, 82);
+            this._closeButton.TabIndex = 0;
+            this._closeButton.Text = "Close";
+            this._closeButton.UseVisualStyleBackColor = true;
+            this._closeButton.Click += new System.EventHandler(this._closeButton_Click);
+            // 
+            // _numlockStatusLabel
+            // 
+            this._numlockStatusLabel.AutoSize = true;
+            this._numlockStatusLabel.Location = new System.Drawing.Point(42, 33);
+            this._numlockStatusLabel.Name = "_numlockStatusLabel";
+            this._numlockStatusLabel.Size = new System.Drawing.Size(360, 29);
+            this._numlockStatusLabel.TabIndex = 2;
+            this._numlockStatusLabel.Text = "I mean.... why is it even a button?";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Will enable numlock any time it is detected to be off.";
+            this.notifyIcon1.BalloonTipTitle = "Nevere NOT Numlock!";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // _runAtStartup
+            // 
+            this._runAtStartup.AutoSize = true;
+            this._runAtStartup.Checked = global::NeverNotNumlock.WinApp.Properties.Settings.Default.NotifyMeWhenDisabled;
+            this._runAtStartup.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::NeverNotNumlock.WinApp.Properties.Settings.Default, "NotifyMeWhenDisabled", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this._runAtStartup.Location = new System.Drawing.Point(47, 92);
+            this._runAtStartup.Name = "_runAtStartup";
+            this._runAtStartup.Size = new System.Drawing.Size(203, 33);
+            this._runAtStartup.TabIndex = 1;
+            this._runAtStartup.Text = "Run at startup?";
+            this._runAtStartup.UseVisualStyleBackColor = true;
+            this._runAtStartup.CheckedChanged += new System.EventHandler(this._runAtStartup_CheckedChanged);
+            // 
+            // _hideButton
+            // 
+            this._hideButton.Location = new System.Drawing.Point(620, 33);
+            this._hideButton.Name = "_hideButton";
+            this._hideButton.Size = new System.Drawing.Size(181, 82);
+            this._hideButton.TabIndex = 3;
+            this._hideButton.Text = "Hide";
+            this._hideButton.UseVisualStyleBackColor = true;
+            this._hideButton.Click += new System.EventHandler(this._hideButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 150);
+            this.Controls.Add(this._hideButton);
+            this.Controls.Add(this._numlockStatusLabel);
+            this.Controls.Add(this._runAtStartup);
+            this.Controls.Add(this._closeButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Never Not Numlock!";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.SizeChanged += new System.EventHandler(this.MainForm_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _closeButton;
+        private System.Windows.Forms.CheckBox _runAtStartup;
+        private System.Windows.Forms.Label _numlockStatusLabel;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button _hideButton;
+    }
+}
+
---
+++ Never-Not-Numlock/MainForm.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        private const int C_MS = 1;
+        private const int C_SECOND = 1000 * C_MS;
+        private const string C_KEYNAME = "NeverNotNumlock";
+
+        public bool IsClosed { get; private set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                this.MonitorNumlock();
+            });
+        }
+
+        private void MonitorNumlock()
+        {
+            while (!this.IsClosed)
+            {
+                this.UpdateState();
+                Thread.Sleep(250 * C_MS);
+            }
+        }
+
+        private void UpdateState()
+        {
+            if (this.InvokeRequired) this.Invoke(new Action(this.UpdateState));
+            else
+            {
+                if (Console.NumberLock) _numlockStatusLabel.Text = "I mean... why is it even a button?";
+                else
+                {
+                    _numlockStatusLabel.Text = "WAIT - WHY IS THE NUMLOCK OFF???";
+                    this.ShowNeverNotNumlock();
+                }
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            NeverNotNumlock.WinApp.Properties.Settings.Default.Save();
+            this.IsClosed = true;
+            base.OnClosed(e);
+        }
+
+        private void _closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainForm_SizeChanged(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = this.WindowState != FormWindowState.Minimized;
+            this.notifyIcon1.Visible = !this.ShowInTaskbar;
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            ShowNeverNotNumlock();
+        }
+
+        private void ShowNeverNotNumlock()
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.BringToFront();
+        }
+
+        private void _hideButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void _runAtStartup_CheckedChanged(object sender, EventArgs e)
+        {
+            // The path to the key where Windows looks for startup applications
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey(
+                                @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+
+            if (_runAtStartup.Checked)
+            {
+                //Path to launch shortcut
+                string startPath = Environment.GetFolderPath(Environment.SpecialFolder.Programs)
+                                   + @"\EffortlessAPI\Windows Tools\Never Not Numlock.appref-ms";
+
+                rkApp.SetValue(C_KEYNAME, startPath);
+            }
+            else
+            {
+                rkApp.DeleteValue(C_KEYNAME);
+            }
+        }
+    }
+}
